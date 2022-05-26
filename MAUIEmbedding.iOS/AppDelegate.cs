db7abc50e24f6117457fe2d78059c901f2cf440c
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-05-26 22:30:05 +0200

    trying to get iOS to work

--- MAUIEmbedding.iOS/AppDelegate.cs
+++ MAUIEmbedding.iOS/AppDelegate.cs
@@ -3,6 +3,8 @@ using Microsoft.Maui.Controls;
 using Microsoft.Maui;
 using UIKit;
 using Microsoft.Maui.Embedding;
+using MAUIEmbedding.Pages;
+using Microsoft.Maui.Platform;
 
 namespace MAUIEmbedding.iOS
 {
@@ -19,31 +21,12 @@ namespace MAUIEmbedding.iOS
 
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
-            //Setup MauiBits
-            var builder = MauiApp.CreateBuilder();
-
-            //Add Maui Controls
-            builder.UseMauiEmbedding<Application>();
-
-            //iOS/Mac need to register the Window
-            builder.Services.Add(new Microsoft.Extensions.DependencyInjection.ServiceDescriptor(typeof(UIKit.UIWindow), Window));
-
-            var mauiApp = builder.Build();
-
-            //Create and save a Maui Context. This is needed for creating Platform UI
-            MauiContext = new MauiContext(mauiApp.Services);
-
-
-
-            // TODO: Create MauiPage here and set as RootViewController?
-
-
-
             // create a new window instance based on the screen size
             Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
             // create a UIViewController with a single UILabel
             var vc = new UIViewController();
+
             vc.View!.AddSubview(new UILabel(Window!.Frame)
             {
                 BackgroundColor = UIColor.SystemBackground,
@@ -51,10 +34,34 @@ namespace MAUIEmbedding.iOS
                 Text = "Hello, iOS!",
                 AutoresizingMask = UIViewAutoresizing.All,
             });
+
             Window.RootViewController = vc;
 
             // make the window visible
             Window.MakeKeyAndVisible();
+
+
+            //Setup MauiBits
+            var builder = MauiApp.CreateBuilder();
+
+            //Add Maui Controls
+            builder.UseMauiEmbedding<Application>();
+
+            //iOS/Mac need to register the Window
+            builder.Services.Add(new Microsoft.Extensions.DependencyInjection.ServiceDescriptor(typeof(UIKit.UIWindow), Window!));
+
+            var mauiApp = builder.Build();
+
+            //Create and save a Maui Context. This is needed for creating Platform UI
+            MauiContext = new MauiContext(mauiApp.Services);
+
+            //Create a Maui Page
+            var myMauiPage = new MainPage();
+
+            //Turn the Maui page into an iOS UIView
+            var view = myMauiPage.ToPlatform(MauiContext);
+
+            //vc.View!.AddSubview(view);
 
             return true;
         }
